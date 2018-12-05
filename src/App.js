a46cbd306445307a50450c2c9640eccc7ec0292b
Anri Liy <anri@realquantum.com>
2018-12-04 20:09:47 -0600

    add css modules

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
-import './App.css';
-
-import Person from './Person/Person';
+import classes from'./App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   state = {
@@ -71,25 +70,24 @@ class App extends Component {
       style.backgroundColor = 'red'
     }
 
-    const classes = []
+    const assignedClasses = [];
     if(this.state.persons.length <=2) {
-      classes.push('red');
+      assignedClasses.push( classes.red );
     } 
     if(this.state.persons.length <=1){
-      classes.push('bold');
+      assignedClasses.push( classes.bold );
     }
 
      return (
-       <StyleRoot>
-       <div className="App">
+    
+       <div className={classes.App}>
          <h1>Hi, I'm a React App</h1>
-         <p className={classes.join(' ')}>This is really working!</p>
+         <p className={assignedClasses.join(' ')}>This is really working!</p>
          <button 
            style = {style}
            onClick={this.togglePersonHandler}>Toggle Persons</button> 
           {persons}  
        </div>
-       </StyleRoot>
      );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'Hi, I\'m a React App'));
   }
