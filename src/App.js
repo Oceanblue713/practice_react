af8d477b7aa26d4e038fa425c01f3b7a416c5d4e
Anri Liy <anri@realquantum.com>
2018-12-09 18:45:11 -0600

    Add ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import classes from'./App.css';
 import Person from './Person/Person'
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 
 class App extends Component {
   state = {
@@ -43,31 +44,26 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-    };
-
+    
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
         <div>
           {this.state.persons.map((person,index) => {
-            return <Person 
-              click = {() => this.deletePersonHandler(index)}
-              name = {person.name}
-              age = {person.name} 
-              key = {person.id}
-              changed = {(event) => this.nameChangedHandler(event, person.id)}/>
+            return <ErrorBoundary key={person.id}>
+              <Person 
+                click = {() => this.deletePersonHandler(index)}
+                name = {person.name}
+                age = {person.name} 
+                key = {person.id}
+                changed = {(event) => this.nameChangedHandler(event, person.id)}/>
+              </ErrorBoundary>
           })}
         </div> 
       );
-      style.backgroundColor = 'red'
+      btnClass = classes.Red;
     }
 
     const assignedClasses = [];
@@ -84,7 +80,7 @@ class App extends Component {
          <h1>Hi, I'm a React App</h1>
          <p className={assignedClasses.join(' ')}>This is really working!</p>
          <button 
-           style = {style}
+           className = {btnClass}
            onClick={this.togglePersonHandler}>Toggle Persons</button> 
           {persons}  
        </div>
